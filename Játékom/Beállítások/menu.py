37d7d4bd571cb324dd89c6391e168e1e90989867
kiralygergelyckik <122787415+kiralygergelyckik@users.noreply.github.com>
2024-06-22 14:08:34 +0200

    Update menu.py

--- Játékom/Beállítások/menu.py
+++ Játékom/Beállítások/menu.py
@@ -1,4 +1,6 @@
+import os
 def bevezetes():
+    os.system('cls')
     print()
     print()
     print("Üdvözöllek!")
@@ -7,14 +9,20 @@ def bevezetes():
     print("Készen állsz?")
     print("A folytatáshoz nyomj egy 1-est!")
 
-def kezdes():
-    print("Egyszer, réges-régen, élt egy ember, kinek neve Gergely volt.")
-    print("Körülbelül, 1617-ben, március 14-én, 11 óra, 12. percében és annak 3. másodpercében, éppen a házimunkáját csinálta, amit megunt.")
-    print("Így hát kalandra indult, de vajon hova?")
-    print("Te hova mennél a helyébe? (Számokkal jelöld válaszod)")
-    print()
-    print("1 - Tovább csinálom a házimunkát.")
-    print("2 - Elmegyek, körbenézek a közeli faluban.")
-    print("3 - Beállok szerzetesnek.")
-    print("4 - Bemegyek a házamba.")
-    print("5 - Megkeresem a barátaimat, hátha van kedvük valamit csinálni.")
+def kezdes(a=0, b=0, falu=0, pap=0, haz=0, barat=0):
+    if b == 0:
+        print("Egyszer, réges-régen, élt egy ember, kinek neve Gergely volt.")
+        print("Körülbelül, 1617-ben, március 14-én, 11 óra, 12. percében és annak 3. másodpercében, éppen a házimunkáját csinálta, amit megunt.")
+        print("Így hát kalandra indult, de vajon hova?")
+        print("Te hova mennél a helyébe? (Számokkal jelöld válaszod)")
+    if a == 0:
+        print()
+        print("1 - Tovább csinálom a házimunkát.")
+        if falu==0:
+            print("2 - Elmegyek, körbenézek a közeli faluban.")
+        if pap==0:    
+            print("3 - Beállok szerzetesnek.")
+        if haz==0:
+            print("4 - Bemegyek a házamba.")
+        if barat==0:
+            print("5 - Megkeresem a barátaimat, hátha van kedvük valamit csinálni.")
